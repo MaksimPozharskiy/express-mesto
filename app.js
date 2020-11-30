929466db50d975ae1b7d45b09b91875a9fc71931
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2020-11-30 20:34:25 +0300

    feat: добавить роутер cards

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use('/', usersRouter);
+app.use('/', cardsRouter);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
