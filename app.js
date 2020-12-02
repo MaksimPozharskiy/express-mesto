e0ede90ad0428252bacfbebc8173ed31c1940697
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2020-12-02 21:29:18 +0300

    refactor: переписать код согласно конфигу EsLint

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
-app.use('*', (req, res) => {
-  return res.status(404).send({ "message": "Запрашиваемый ресурс не найден" })
-});
+app.use('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Сервер запущен на порту ${PORT}`);
 });
